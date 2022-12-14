a0aa2532fbe2213dd5eecdbd9d6b0e762dc21886
Gihovani Filipp Pereira Demetrio <gihovani@gmail.com>
2022-12-14 20:26:08 -0300

    codigo da aula 02

---
+++ src/app/entities/notification.spec.ts
@@ -0,0 +1,43 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('Notification', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade.'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notification).toBeTruthy();
+  });
+  it('should be able to set and get notification variables', () => {
+    const notificationCreatedAt = new Date();
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade.'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+      createdAt: notificationCreatedAt,
+    });
+
+    const notificationContent = 'Nova solicitação de amizade2.';
+    notification.content = new Content(notificationContent);
+    expect(notification.content.value).toEqual(notificationContent);
+
+    const notificationCategory = 'test';
+    notification.category = notificationCategory;
+    expect(notification.category).toEqual(notificationCategory);
+
+    const notificationRecipientId = 'recipient-id';
+    notification.recipientId = notificationRecipientId;
+    expect(notification.recipientId).toEqual(notificationRecipientId);
+
+    const notificationReadAt = null;
+    notification.readAt = notificationReadAt;
+    expect(notification.readAt).toBeNull();
+
+    expect(notification.createdAt).toEqual(notificationCreatedAt);
+
+    expect(notification.id).toBeTruthy();
+  });
+});
